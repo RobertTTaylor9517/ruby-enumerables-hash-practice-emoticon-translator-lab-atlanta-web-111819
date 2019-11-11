455d34045613419ba9e98741b78365816f2d904c
inherited-window-0818 <robt15@hotmail.com>
2019-11-11 20:06:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,12 +1,12 @@
 # require modules here
 require "yaml"
 
-def load_library
+def load_library(path)
   mot_lib = {"meaning" => {}, 
   "emoticon" => {}
   }
   
-  YAML.load_file('emoticons.yml').each {|meaning, japmeaning|
+  YAML.load_file(path).each {|meaning, japmeaning|
     eng, jap = japmeaning
     mot_lib[meaning][jap] = meaning
     mot_lib[emoticon][eng] = jap
